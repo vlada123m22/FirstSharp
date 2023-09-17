71325217e0e34f74b608009a58ddc7c8505a2799
Vladislava Musin <vlada.musin@gmail.com>
2023-09-17 12:13:45 +0300

    first commit

---
+++ FirstProject/Program.cs
@@ -0,0 +1,32 @@
+﻿class Car
+{
+    private string color;
+    private int speed;
+    private string model;
+    private string manufacturer;
+
+    public string Color { get => color; set => color = value; }
+    public int Speed { get => speed; set => speed = value; }
+    public string Model { get => model; set => model = value; }
+    public string Manufacturer { get => manufacturer; set => manufacturer = value; }
+
+    public bool IsMooving(int speed)
+    {
+        if (speed > 0) return true; else return false;
+    }
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.Color = "white";
+        Console.WriteLine(car.Color);
+        Console.Write("Introduce speed");
+        car.Speed = int.Parse(Console.ReadLine());
+        if (car.IsMooving(car.Speed)){ Console.WriteLine("Is mooving"); } else { Console.WriteLine("Is not mooving"); }       
+    }
+}
